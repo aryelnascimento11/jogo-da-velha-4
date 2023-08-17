b8791b0e30dcb079d872cda0bd8b85904299d9d8
Aryel <126723967+aryelnascimento11@users.noreply.github.com>
2023-08-17 13:35:22 -0300

    primeiro site

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,30 @@ export default function Home() {
     <>
       <header className='bg-black text-white flex justify-between p-6 place-items-center shadow shadow-black'>
         <h1 className=' text-3xl'>Hello World</h1>
-        <input type="search" className='text-black ' placeholder='Search'/>
-         <nav>
-           <ul className='flex'>
-            <li className='p-2'>
-              <a href="#">exemplo</a>
-            </li>
-            <li className='p-2'>
-              <a href="#">exemplo</a>
-            </li>
-            <li className='p-2'>
-              <a href="#">exemplo</a>
-            </li>
-            <button className='bg-white text-black py-1 px-3 rounded-full hover:bg-blue-500 transi'>entrar</button>
-          </ul>
-        </nav>
+        <div className='flex place-items-center'>
+          <input type="search" className='text-black  rounded-lg p-4 ' placeholder='Search'/>
+          <nav>
+            <ul className='flex'>
+              <li className='p-2'>
+                <a href="#">exemplo</a>
+              </li>
+              <li className='p-2'>
+                <a href="#">exemplo</a>
+              </li>
+              <li className='p-2'>
+                <a href="#">exemplo</a>
+              </li>
+              <button className='bg-white text-black py-1 px-3 rounded-full hover:bg-blue-500 transi'>entrar</button>
+            </ul>
+          </nav>
+        </div>
       </header>
-      <h2 className='text-9xl bg-slate-600 text-center m-10'>Sei lá</h2>
-      <footer className=''> Hello World</footer>
+     <section className='text-center'>
+       <h1 className='text-white text-5xl  p-10'>Fazendo o primeiro site na Eliti</h1>
+       <button className='bg-white text-black py-1 px-3 rounded-full hover:bg-blue-500 transi'>Saiba mais</button>
+     </section>
+    <a href="https://www.instagram.com/aryeln897/"></a>
+    
       
 
     </>
