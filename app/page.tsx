2255effe4a5fc8f92c006a3bbd63badee08e927d
Aryel <126723967+aryelnascimento11@users.noreply.github.com>
2023-08-23 16:51:55 -0300

    create-project

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
        <h1 className='text-white text-5xl  p-10'>Fazendo o primeiro site na Eliti</h1>
        <button className='bg-white text-black py-1 px-3 rounded-full hover:bg-blue-500 transi'>Saiba mais</button>
      </section>
-    <a href="https://www.instagram.com/aryeln897/"></a>
+    
     
       
 
